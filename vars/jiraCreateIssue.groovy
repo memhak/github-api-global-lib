20a877e5bad73f6d6fdde482d6a21a7248aae151
Mehmet Hakan Kurtoğlu <mehmethakankurtoglu@gmail.com>
2023-08-23 17:18:35 +0200

    Update jiraCreateIssue.groovy
    typo

--- vars/jiraCreateIssue.groovy
+++ vars/jiraCreateIssue.groovy
@@ -5,7 +5,7 @@ def call(Map config=[:]) {
     summary: "${config.summary}",
     description: "${config.description}",
     issueTypeName: "${config.issueTypeName}",
-    duedate: "${config.duedata}"
+    duedate: "${config.duedate}"
   ]
   def render = renderTemplate(rawBody,binding)
   sh('curl -D- -u $JIRA_CREDENTIALS -X POST --data "'+render+'" -H "Content-Type: application/json" $JIRA_URL/rest/api/2/issue')
